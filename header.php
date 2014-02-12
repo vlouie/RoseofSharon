5a4ab6b9b655c3d59e8514e5d81082ad4a8b619e
vlouie <v.louie91@gmail.com>
2014-02-11 23:14:54 -0800

    Adding meta tag to try and account for mobile devices

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width" />
 <title>Rose of Sharon Floral Arts - floral arrangements for all occasions</title>
 <meta name="viewport" content="width=device-width">
 <meta name="description" content="Rose of Sharon Floral Arts is a Vancouver-based business specialized in floral arrangements for all occasions">
