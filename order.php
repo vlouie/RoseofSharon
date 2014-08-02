57c62dbfa3de708c0a27f64aea191c82baef94fd
Victoria Louie <v.louie91@gmail.com>
2014-08-02 10:42:15 -0700

    Trying out the Mandrill mailer

--- order.php
+++ order.php
@@ -1,27 +1,54 @@
 <?php
 include('header.php');
+require 'PHPMailerAutoload.php';
 ?>
 <?php
+
 if(isset($_POST['submit'])) {
+  $mail = new PHPMailer;
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->Host = 'smtp.mandrillapp.com';  // Specify main and backup SMTP servers
+  $mail->Port = 587;
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'MANDRILL_USERNAME';                 // SMTP username
+  $mail->Password = 'MANDRILL_APIKEY';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+  $mail->From = 'from@example.com';
+  $mail->FromName = 'Mailer';
+  $mail->addAddress('v.louie91@gmail.com', 'VL');     // Add a recipient
+
+  $mail->IsHTML(true);
+
+  $mail->Subject = 'Here is the subject';
+  $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+  if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+  } else {
+    echo 'Message has been sent';
+  }
   // TODO: customize
-  $to = "v.louie91@gmail.com";
-  $subject = "Rose of Sharon Order Request";
+  //$to = "v.louie91@gmail.com";
+  //$subject = "Rose of Sharon Order Request";
 
-  // data the visitor provided
-  $name_field = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-  $email_field = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-  $phone_field = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
-  $order_field = filter_var($_POST['order'], FILTER_SANITIZE_STRING);
-  $quantity_field = filter_var($_POST['quantity'], FILTER_SANITIZE_STRING);
-  $comment = filter_var($_POST['comment'], FILTER_SANITIZE_STRING);
+  //// data the visitor provided
+  //$name_field = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+  //$email_field = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+  //$phone_field = filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
+  //$order_field = filter_var($_POST['order'], FILTER_SANITIZE_STRING);
+  //$quantity_field = filter_var($_POST['quantity'], FILTER_SANITIZE_STRING);
+  //$comment = filter_var($_POST['comment'], FILTER_SANITIZE_STRING);
 
-  //constructing the message
-  $body = " From: $name_field\n\n E-Mail: $email_field\n\n Phone: $phone_field\n\n  Order: $order_field\n\n Quantity: $quantity_field\n\n Notes/Comments:\n\n $comment";
+  ////constructing the message
+  //$body = " From: $name_field\n\n E-Mail: $email_field\n\n Phone: $phone_field\n\n  Order: $order_field\n\n Quantity: $quantity_field\n\n Notes/Comments:\n\n $comment";
 
-  mail($to, $subject, $body);
+  //mail($to, $subject, $body);
 
-  echo "<script>alert('Order successfully processed. You will hear back from us in the next few days.');</script>";
-  sleep(100);
+  //echo "<script>alert('Order successfully processed. You will hear back from us in the next few days.');</script>";
+  //sleep(100);
   // redirect to confirmation
   header('Location: index.php');
 } else {
