2bcf7b23a55f6c57fee7bc16c1e5b15f91199302
Victoria Louie <v.louie91@gmail.com>
2014-08-03 12:40:49 -0700

    Trying to add buttons to homepage and work on styling for title banners

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $(window).load(function() {
 });
 </script>
 
-<!-- 
+<!--
 <script src="js/jquery.slides.min.js"></script>
  <script>
     $(function() {
@@ -51,15 +51,67 @@ $(window).load(function() {
 	<!--<div class="container">
 	</div>-->
 	<h3>Welcome</h3>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate est porta enim mollis, aliquet vehicula magna dignissim. Etiam eget libero at ligula consectetur rutrum ac vitae mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis commodo sapien at consequat euismod. Praesent eu mauris ultricies, ornare massa posuere, facilisis purus. Ut consequat urna non rhoncus condimentum. Cras dapibus est nec erat convallis, at imperdiet sapien mollis. Proin sodales erat in augue tincidunt mattis. Vivamus sed velit lorem. Mauris ac rutrum elit. Aliquam tempor ipsum vitae nunc vestibulum, id varius eros convallis. Donec fringilla neque ut aliquet vulputate. Morbi a dui in tellus rutrum volutpat quis ac elit. Sed dignissim vestibulum neque, ut posuere urna vestibulum quis. Nam feugiat mi libero. Morbi vitae dictum nisl.
-
-  Morbi vel pharetra tortor, vitae pulvinar felis. Mauris vitae leo tortor. Praesent at ante magna. Maecenas id consectetur libero, et gravida nulla. Praesent consectetur, ligula in lobortis vehicula, massa purus consequat felis, ut sollicitudin lacus purus ac augue. Ut quis libero non eros luctus viverra eu in est. Sed faucibus ligula et bibendum vestibulum. Duis tincidunt mauris facilisis, blandit neque vel, facilisis mauris. Aliquam bibendum sapien id metus egestas eleifend. Donec sem sem, dapibus nec imperdiet non, pretium ac felis. Phasellus in pulvinar risus, vitae semper tortor. Morbi facilisis fringilla justo id sagittis.
-
-  Etiam pellentesque, lorem id hendrerit fermentum, risus dolor dignissim turpis, ultrices ornare lectus libero vitae dui. Integer rhoncus urna vitae sagittis gravida. Praesent consectetur tortor sit amet ante consequat vestibulum. Nunc hendrerit porta sem, quis feugiat lacus placerat at. Mauris sit amet scelerisque velit. Mauris non tristique elit, a tristique risus. Etiam lobortis nisl nec nibh accumsan posuere. Maecenas a justo accumsan, faucibus urna non, aliquam lacus. Proin malesuada metus non scelerisque imperdiet. Quisque vitae tempus urna. Pellentesque id mauris at dolor accumsan mattis id nec libero. Integer ornare quam sit amet aliquet malesuada. Nulla fermentum gravida lacinia. Nullam placerat quis magna vel mollis.
-
-  Cras volutpat diam id augue semper condimentum. Vestibulum sit amet consectetur risus, sed bibendum tellus. Morbi ut urna a felis semper vestibulum. Nunc in mi consequat enim accumsan euismod in eget metus. Aliquam eleifend pharetra dui, in pretium eros luctus id. Phasellus sed pretium justo. Quisque turpis est, vestibulum nec imperdiet in, eleifend ut massa. Praesent pellentesque condimentum laoreet. Integer imperdiet sapien non nisl auctor malesuada a id lectus. Aliquam vitae diam erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-  Nunc in diam ac neque eleifend bibendum at semper eros. Donec viverra odio et purus placerat ullamcorper. Suspendisse blandit risus nunc, a vehicula velit fermentum rutrum. Proin blandit suscipit massa eu feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut vitae imperdiet mauris, ut pretium dui. Duis quis imperdiet augue. Donec sed egestas nisi. Morbi vitae neque nibh. 
+  <table>
+    <tr>
+      <td>
+        <a href="/christmas.php">
+          <div id="christmasButton">Christmas</div>
+        </a>
+      </td>
+      <td>
+        <a href="/easter.php">
+          <div id="easterButton">Easter</div>
+        </a>
+      </td>
+      <td>
+        <a href="/mothers_day.php">
+          <div id="mothersButton">Mother's Day</div>
+        </a>
+      </td>
+      <td>
+        <a href="/thanksgiving.php">
+          <div id="thanksgivingButton">Thanksgiving</div>
+        </a>
+      </td>
+      <td>
+        <a href="/valentines.php">
+          <div id="valentinesButton">Valentine's</div>
+        </a>
+      </td>
+      <td>
+        <a href="/weddings.php">
+          <div id="weddingsButton">Weddings</div>
+        </a>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <a href="/corsage.php">
+          <div id="corsageButton">Corsages</div>
+        </a>
+      </td>
+      <td>
+        <a href="/posy.php">
+          <div id="posyButton">Posies</div>
+        </a>
+      </td>
+      <td>
+        <a href="/high_style.php">
+          <div id="highstyleButton">High Style</div>
+        </a>
+      </td>
+      <td>
+        <a href="/event.php">
+          <div id="eventButton">Events</div>
+        </a>
+      </td>
+      <td>
+        <a href="/special_order.php">
+          <div id="specialButton">Special Order</div>
+        </a>
+      </td>
+    </tr>
+  </table>
 </div>
 <?php
 include('footer.php');
