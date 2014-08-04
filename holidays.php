e167d43f5be856265a848b7bbcdd23d8388b4f72
Victoria Louie <v.louie91@gmail.com>
2014-08-04 03:40:57 -0700

    Fixing typo in class and fixing margin

--- holidays.php
+++ holidays.php
@@ -2,7 +2,7 @@
 include('header.php');
 ?>
 <div id="content">
-<h3 id="holidayBanner">Holidays</h3>
+<h3 id="holidaysBanner">Holidays</h3>
 We arrange flowers for a variety of occasions, including several holidays.  Arrangements for specific holidays can also feature characteristics unique to that holiday.  Some of the holidays we have done arrangements for previously include:
 <ul>
   <li><a href="christmas.php">Christmas</a>
