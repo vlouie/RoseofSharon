1fe403fe458897b870335dbfc868ed7290807309
vlouie <v.louie91@gmail.com>
2014-08-07 01:11:43 -0700

    Styling the wedding page, tweaking the banner style

--- wedding.php
+++ wedding.php
@@ -3,23 +3,24 @@ include('header.php');
 ?>
 <script src="js/table.js" type="text/javascript"></script>
 <script type="text/javascript">
-  $(document).ready(function(){
+  /*$(document).ready(function(){
     tablize($('#wedding'), lightbox);
-  });
+  });*/
 </script>
 <div id="content">
-<h3 id="weddingBanner">Weddings</h3>
-<!-- insert partial image
-<table id="wedding" data-csv="wedding">
-  <tbody>
-  </tbody>
-</table> -->
-In order to create the perfect wedding experience for you, we price our products and arrangements according to the unique nature of the event.
-<h4>Abraham and Janet's Wedding</h4>
-<img src="img/wedding/gifs/centerpieces.gif">
-<img src="img/wedding/gifs/corsages.gif">
-<img src="img/wedding/bouquet01.png">
-<img src="img/wedding/centerpiece01.png">
+  <h3 id="weddingBanner">Weddings</h3>
+  In order to create the perfect wedding experience for you, we price our products and arrangements according to the unique nature of the event. Below are some examples of arrangements we have designed for weddings in the past.
+
+  <h4>Abraham and Janet's Wedding (Sept 2013)</h4>
+  <div class = "wedding-column">
+    <img class = "wedding-fitted" src="img/wedding/gifs/centerpieces.gif" />
+    <img class = "wedding-fitted" src="img/wedding/bouquet01.png" />
+  </div>
+  <div class = "wedding-column">
+    <img class = "wedding-fitted" src="img/wedding/centerpiece01.png" />
+    <img class = "wedding-fitted" src="img/wedding/gifs/corsages.gif" />
+  </div>
+  Photo credit: <a href="http://videowedding.ca/" target="_blank">VideoWeddingProductions</a> (<a href="http://v.youku.com/v_show/id_XNjIxMTI0MDQ0.html" target="_blank">Source</a>)
 </div>
 <?php
 include('footer.php');
