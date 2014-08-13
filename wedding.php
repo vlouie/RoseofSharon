613b5613b45ff191d5fa5207eaafed69b1ca7945
vlouie <v.louie91@gmail.com>
2014-08-13 00:07:34 -0700

    Adding more pictures to the wedding page, tweaking styling to account for this

--- wedding.php
+++ wedding.php
@@ -2,25 +2,29 @@
 include('header.php');
 ?>
 <script src="js/table.js" type="text/javascript"></script>
-<script type="text/javascript">
-  /*$(document).ready(function(){
-    tablize($('#wedding'), lightbox);
-  });*/
-</script>
 <div id="content">
   <h3 id="weddingBanner">Weddings</h3>
   In order to create the perfect wedding experience for you, we price our products and arrangements according to the unique nature of the event. Below are some examples of arrangements we have designed for weddings in the past.
 
   <h4>Abraham and Janet's Wedding (Sept 2013)</h4>
   <div class = "wedding-column">
-    <img class = "wedding-fitted" src="img/wedding/gifs/centerpieces.gif" />
     <img class = "wedding-fitted" src="img/wedding/bouquet01.png" />
+    <img class = "wedding-fitted" src="img/wedding/gifs/centerpieces.gif" />
+    <img class = "wedding-fitted" src="img/wedding/corsage01.png" />
   </div>
   <div class = "wedding-column">
     <img class = "wedding-fitted" src="img/wedding/centerpiece01.png" />
-    <img class = "wedding-fitted" src="img/wedding/gifs/corsages.gif" />
+    <img class = "wedding-fitted" src="img/wedding/corsage02.png" />
+    <img class = "wedding-fitted" src="img/wedding/bouquet02.png" />
   </div>
-  Photo credit: <a href="http://videowedding.ca/" target="_blank">VideoWeddingProductions</a> (<a href="http://v.youku.com/v_show/id_XNjIxMTI0MDQ0.html" target="_blank">Source</a>)
+  <div class = "wedding-column">
+    <img class = "wedding-fitted" src="img/wedding/centerpiece02.png" />
+    <img class = "wedding-fitted" src="img/wedding/gifs/corsages.gif" />
+    <img class = "wedding-fitted" src="img/wedding/bouquet03.png" />
+  </div>
+  <sub>
+    Photo credit: <a href="http://videowedding.ca/" target="_blank">VideoWeddingProductions</a> (<a href="http://v.youku.com/v_show/id_XNjIxMTI0MDQ0.html" target="_blank">Source</a>)
+  </sub>
 </div>
 <?php
 include('footer.php');
